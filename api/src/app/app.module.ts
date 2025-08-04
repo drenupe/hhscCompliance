46a69a44bd11070ce8a75e87f8dab866f5636c8e
drenupe <mccaskillhld6@gmail.com>
2025-08-04 16:05:17 -0500

    addedPostgressConnection

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '../../config/typeorm.config';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forFeature([User]),
+],
   controllers: [AppController],
   providers: [AppService],
 })
