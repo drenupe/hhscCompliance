c7e094f84c065d4faf433e73a5dac04f50013297
drenupe <mccaskillhld6@gmail.com>
2025-12-22 20:46:55 -0600

    fix(server): update build output path in webpack config and streamline project.json configurations

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -2,8 +2,9 @@ const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 
 module.exports = {
+  // Force build output to workspace-root: /dist/api
   output: {
-    path: join(__dirname, '../dist/server'),
+    path: join(__dirname, '../dist/api'),
     filename: 'main.js',
   },
   plugins: [
@@ -13,7 +14,10 @@ module.exports = {
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
       assets: ['./src/assets'],
+
+      // Keep your current behavior. (If you want prod minification later, set true.)
       optimization: false,
+
       outputHashing: 'none',
       generatePackageJson: true,
     }),
