d58903cd2c699cbaa7ca3a85a5f3c38bf8297f77
drenupe <mccaskillhld6@gmail.com>
2025-12-24 14:11:15 -0600

    add styles.scss

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import './styles.scss';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppShellComponent, appConfig } from '@hhsc-compliance/web-shell';
-
 bootstrapApplication(AppShellComponent, appConfig).catch(console.error);
